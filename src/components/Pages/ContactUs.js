939bf10f650612bc432cce94726c1f3e4bd94364
AnjanaShreya <chslash01@yahoo.com>
2024-12-02 15:07:25 +0530

    commit

--- src/components/Pages/ContactUs.js
+++ src/components/Pages/ContactUs.js
@@ -1,36 +1,26 @@
 import React from "react";
 import img2 from "../../assets/img2.jpg";
-// import img0 from '../../assets/img0.jpg'
 import Navbar from "../Navbar.js/Navbar";
 import Footer from "../Navbar.js/Footer";
 
-const Internships = () => {
+const ContactUs = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="relative">
+      <div className="relative z-20">
+        <Navbar />
+      </div>
       <div className="relative">
         {/* Image */}
-        <img src={img2} alt="Internship" className="w-full h-72 object-cover" />
+        <img src={img2} alt="Contactus" className="w-full h-72 object-cover" />
 
         {/* Overlay */}
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center p-28">
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center p-28 z-10">
           <h1 className="text-white text-3xl font-bold">Contact Us</h1>
         </div>
-      </div>
-      <div>
-        {/* Content here */}
-        content here<br/>
-        content here<br/>
-        content here<br/>
-        content here<br/>
-        content here<br/>
-        content here<br/>
-        content here<br/>
-        content here<br/>
       </div>
       <Footer />
     </div>
   );
 };
 
-export default Internships;
+export default ContactUs;
